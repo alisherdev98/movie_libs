f9d945b8418faf0de49a0b85422bf2efa24ddf8b
alisherdev98 <alisherbaidybekov@gmail.com>
2024-07-24 23:56:32 +0500

    fix paths movie and movie subapp

--- movie_test/urls.py
+++ movie_test/urls.py
@@ -2,12 +2,15 @@ from django.contrib import admin
 from django.urls import path, include
 
 from movies.urls import movies_urlpatterns as movie_urls, directors_urlpatterns as directors_urls
-
+from movies.genres.urls import urlpatterns as genres_urls
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/', include([
-        path('movies/', include(movie_urls)),
-        path('directors/', include(directors_urls)),
-        ])
-    )
+        path('movies/', include([
+            path('directors/', include(directors_urls)),
+            path('genres/', include(genres_urls)),
+            path('', include(movie_urls)),  # TODO: why if empty path will be above, suppath dont find
+        ])),
+        ])),
 ]
+
--- movies/urls.py
+++ movies/urls.py
@@ -2,14 +2,15 @@ from django.urls import path
 from rest_framework import routers
 
 from movies.views import MovieViewSet, DirectorViewSet
+from movies.genres.urls import urlpatterns as genres_urlpatterns
 
-movies_router = routers.DefaultRouter()
-movies_router.register('', MovieViewSet)
-
-movies_urlpatterns = movies_router.urls
 
 directors_router = routers.DefaultRouter()
 directors_router.register('', DirectorViewSet)
 
 directors_urlpatterns = directors_router.urls
 
+movies_router = routers.DefaultRouter()
+movies_router.register('', MovieViewSet)
+
+movies_urlpatterns = movies_router.urls
